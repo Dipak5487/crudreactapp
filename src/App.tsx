5947faaaecd3188cc0026f0d28abddbb4aca54ad
Dipak5487 <dipakchauhan330@gmail.com>
2023-02-26 15:19:16 +0530

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from scracth here!
+          Learn React from scracth here........!
         </a>
       </header>
     </div>

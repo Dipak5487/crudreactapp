be6edaed1eee3a80b9e59026c06a70581496099b
Dipak5487 <dipakchauhan330@gmail.com>
2023-03-05 23:28:08 +0530

    added table and material ui

--- src/Services/Create.tsx
+++ src/Services/Create.tsx
@@ -1,56 +1,101 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
+import { Input } from '@mui/material';
 
-const Create= ()=>{
-    const [title, setTitle] = useState('');
-const [body, setBody] = useState('');
-const [items, setData]:any = useState([]);
 
-  const addPosts = async (title:any, body:any) => {
-    await fetch('https://dummyjson.com/posts/add', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          title: title,
-          userId: 5,
-          /* other post data */
-        })
+interface users {
+   name: string,
+   dob: string,
+   emailId: string,
+   country: string,
+   mobileNumber: string
+}
+const Create = () => {
+   const [user, setUser] = useState({ id:0,name: '', dob: '', emailId: '', country: '', mobileNumber: '' });
+
+   //     const [name, setName] = useState('');
+   // const [dob, setDob] = useState('');
+   // const [emailId, setEmail] = useState('');
+   // const [country, setCountry] = useState('');
+   // const [mobileNumber, setMobile] = useState('');
+   const [items, setData]: any = useState([]);
+
+   const addPosts = async (user: any) => {
+      console.log(user)
+      await fetch('https://localhost:44396/api/User/user-create', {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify(user)
       })
-     
-       .then((response) => response.json())
-       .then((data) => {
-        setData(data);
-        console.log("seetrtr",data)
-          setTitle('');
-          setBody('');
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- };
-  const handleSubmit = (e : any) => {
-    e.preventDefault();
-    addPosts(title, body);
- }; 
-    return (
-        <div className="app">
-           <div className="add-post-container">
-              <form onSubmit={handleSubmit}>
-                 <input type="text" className="form-control" value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                 />
-                 <textarea name="" className="form-control" id="" 
-                    value={body} onChange={(e) => setBody(e.target.value)} 
-                 ></textarea>
-                 <button type="submit">Add Post</button>
-              </form>
-           </div>
 
-           <h1>{items.id}</h1>
-           <h1>{items.title}</h1>
-           <h1>{items.userId}</h1>
-           <h1>{items.body}</h1>
-        </div>
-     );
+         .then((response) => response.json())
+         .then((data) => {
+            setData(data);
+            console.log("seetrtr", data)
+            //   setName('');
+            //     setDob('');
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   };
+
+   const handleSubmit = (e: any) => {
+      e.preventDefault();
+      addPosts(user);
+   };
+   return (
+      <div className="app">
+         <div className="add-post-container">
+            <form onSubmit={handleSubmit}>
+            <label htmlFor='name'>Name</label>
+               <Input type="text" className="form-control" id ='name'value={user.name}
+                  onChange={(e) => setUser({
+                     ...user,
+                     name: e.target.value
+                  })}
+               />
+               <br></br>
+               <label htmlFor='dob'>Date of Birth</label>
+               <Input name="dob" className="form-control" id="dob"
+                  value={user.dob} onChange={(e) => setUser({
+                     ...user,
+                     dob: e.target.value
+                  })}
+               />
+               <br></br>
+               <label htmlFor='emailId'>Email Id</label>
+               <Input name="emailId" className="form-control" id="emailId"
+                  value={user.emailId} onChange={(e) => setUser({
+                     ...user,
+                     emailId: e.target.value
+                  })}
+               />
+               <br></br>
+               <label htmlFor='country'>Country Name</label>
+               <Input name="country" className="form-control" id="country"
+                  value={user.country} onChange={(e) => setUser({
+                     ...user,
+                     country: e.target.value
+                  })}
+               />
+               <br></br>
+               <label htmlFor='mobileNumber'>Mobile Number</label>
+               <Input name="mobileNumber" className="form-control" id="mobileNumber"
+                  value={user.mobileNumber} onChange={(e) => setUser({
+                     ...user,
+                     mobileNumber: e.target.value
+                  })} />
+                  <Button variant="outlined" type ="submit" size='large'>Create</Button>
+            </form>
+         </div>
+
+         <h1>{items.id}</h1>
+         <h1>{items.title}</h1>
+         <h1>{items.userId}</h1>
+         <h1>{items.body}</h1>
+      </div>
+   );
 }
 
 export default Create
--- src/Services/GetData.tsx
+++ src/Services/GetData.tsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+function Add(number1: any, number2: any) {
+   return number1 + number2;
+}
 
-
-
-const GetData =() =>
-{
-    const [items, setPosts]:any = useState([]);
+const GetData = () => {
+   const [items, setPosts]: any = useState([]);
    useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      fetch('https://localhost:44396/api/User/get-all')
          .then((response) => response.json())
          .then((data) => {
-             setPosts(data);
-             console.log(data);
+            setPosts(data);
+            console.log("get locall date", data);
          })
          .catch((err) => {
             console.log(err.message);
@@ -18,34 +26,52 @@ const GetData =() =>
    }, []);
 
 
-   
 
-  function handleDelete(e:any)
-  {
-   console.log("vale herer of e",e)
-   fetch(`https://dummyjson.com/posts/${e}`,{
-      method: 'DELETE'
-   })
-   .then(res => res.json())
-.then(console.log);
 
-  }
+   function handleDelete(e: any) {
+      console.log("vale herer of e", e)
+      fetch(`https://localhost:44396/api/User/${e}`, {
+         method: 'DELETE'
+      })
+         .then(res => res.json())
+         .then(console.log);
 
-   return(
-    <div className="posts-container">
-      {items.map((item:any) => {
-         return (
-            <div >
-                <h4>{item.id} </h4>
-                <button onClick={()=>handleDelete(item.id)}>DELETE</button>
-                  <h4>{item.body}</h4>
-                <h4>{item.title}</h4>
-                <h4>{item.userId}</h4>
-            </div>
-         );
-      })}
-   </div>
-   )
+   }
+
+   return (
+      <div className="posts-container">
+
+         <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 350 }} aria-label="simple table">
+               <TableHead>
+                  <TableRow>
+                     <TableCell>User Id</TableCell>
+                     <TableCell align="right">User name</TableCell>
+                     <TableCell align="right">Date of Birth</TableCell>
+                     <TableCell align="right">Email Id</TableCell>
+                     <TableCell align="right">Country</TableCell>
+                     <TableCell align="right">Mobile Number</TableCell>
+                  </TableRow>
+               </TableHead>
+               <TableBody>
+                  {items.map((item: any) => (
+                     <TableRow
+                        key={item.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                     >
+                        <TableCell align="right">{item.id}</TableCell>
+                        <TableCell component="th" scope="row"> {item.name}</TableCell>
+                        <TableCell align="right">{item.dob}</TableCell>
+                        <TableCell align="right">{item.emailId}</TableCell>
+                        <TableCell align="right">{item.country}</TableCell>
+                        <TableCell align="right">{item.mobileNumber}</TableCell>
+                        <Button variant="contained" color='error' onClick={() => handleDelete(item.id)}> Delete</Button>
+                     </TableRow>
+                  ))}
+               </TableBody>
+            </Table>
+         </TableContainer>
+      </div>
+   );
 }
-
 export default GetData

2dfd07aa2de7eb85542610e8bfc57e18ced085a8
Dipak5487 <dipakchauhan330@gmail.com>
2023-03-08 21:02:05 +0530

    added Chat gpt call

--- src/Services/Create.tsx
+++ src/Services/Create.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import { Input } from '@mui/material';
+import Container from '@mui/material/Container';
+import { margin } from '@mui/system';
+
 
 
 interface users {
@@ -12,14 +15,9 @@ interface users {
 }
 const Create = () => {
    const [user, setUser] = useState({ id:0,name: '', dob: '', emailId: '', country: '', mobileNumber: '' });
-
-   //     const [name, setName] = useState('');
-   // const [dob, setDob] = useState('');
-   // const [emailId, setEmail] = useState('');
-   // const [country, setCountry] = useState('');
-   // const [mobileNumber, setMobile] = useState('');
+   const [question, setQuestion] = useState('')
+   const [answer, setAnswer] = useState('')
    const [items, setData]: any = useState([]);
-
    const addPosts = async (user: any) => {
       console.log(user)
       await fetch('https://localhost:44396/api/User/user-create', {
@@ -31,24 +29,39 @@ const Create = () => {
          .then((response) => response.json())
          .then((data) => {
             setData(data);
-            console.log("seetrtr", data)
-            //   setName('');
-            //     setDob('');
+            console.log("created", data)
          })
          .catch((err) => {
             console.log(err.message);
          });
    };
 
+   const searchOpenAI = async (user: any) => {
+      console.log(" Quesitr is",user)
+     const res = await fetch('https://localhost:44396/api/User/open-ai', {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify(user)
+      })
+      const result = await res.text();
+      console.log(result)
+      setAnswer(result)
+   };
+
    const handleSubmit = (e: any) => {
       e.preventDefault();
       addPosts(user);
    };
+
+   const handleOpenAISubmit = (e: any) => {
+      e.preventDefault();
+     searchOpenAI(question);
+   };
    return (
       <div className="app">
-         <div className="add-post-container">
+         <Container maxWidth="sm" sx={{ p: 2, border: '1px dashed grey'}} style={{marginTop:"20px"}}>
             <form onSubmit={handleSubmit}>
-            <label htmlFor='name'>Name</label>
+            <label htmlFor='name' style={{padding:"25px"}}>Name</label>
                <Input type="text" className="form-control" id ='name'value={user.name}
                   onChange={(e) => setUser({
                      ...user,
@@ -56,7 +69,7 @@ const Create = () => {
                   })}
                />
                <br></br>
-               <label htmlFor='dob'>Date of Birth</label>
+               <label htmlFor='dob' style={{padding:"25px"}}>Date of Birth</label>
                <Input name="dob" className="form-control" id="dob"
                   value={user.dob} onChange={(e) => setUser({
                      ...user,
@@ -64,7 +77,7 @@ const Create = () => {
                   })}
                />
                <br></br>
-               <label htmlFor='emailId'>Email Id</label>
+               <label htmlFor='emailId' style={{padding:"25px"}}>Email Id</label>
                <Input name="emailId" className="form-control" id="emailId"
                   value={user.emailId} onChange={(e) => setUser({
                      ...user,
@@ -72,7 +85,7 @@ const Create = () => {
                   })}
                />
                <br></br>
-               <label htmlFor='country'>Country Name</label>
+               <label htmlFor='country' style={{padding:"25px"}}>Country Name</label>
                <Input name="country" className="form-control" id="country"
                   value={user.country} onChange={(e) => setUser({
                      ...user,
@@ -80,20 +93,41 @@ const Create = () => {
                   })}
                />
                <br></br>
-               <label htmlFor='mobileNumber'>Mobile Number</label>
+               <label htmlFor='mobileNumber' style={{padding:"25px"}}>Mobile Number</label>
                <Input name="mobileNumber" className="form-control" id="mobileNumber"
                   value={user.mobileNumber} onChange={(e) => setUser({
                      ...user,
                      mobileNumber: e.target.value
                   })} />
-                  <Button variant="outlined" type ="submit" size='large'>Create</Button>
+                  <br/>
+                  <Button variant="outlined" type ="submit" size='large' style={{margin:"25px"}}>Create</Button>
             </form>
-         </div>
+         </Container>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
 
-         <h1>{items.id}</h1>
-         <h1>{items.title}</h1>
-         <h1>{items.userId}</h1>
-         <h1>{items.body}</h1>
+         <div>
+         <Container maxWidth="sm" sx={{ p: 2, border: '1px dashed grey' }}>
+            <form onSubmit={handleOpenAISubmit}>
+            <label htmlFor='Question' style={{padding:"25px"}}>Ask Question</label>
+               <Input name="Question" className="form-control" id="Question"
+                  value={question} onChange={(e) => setQuestion( e.target.value)} />
+                  <br/>
+                  <Button variant="outlined" type ="submit" size='large' style={{margin:"25px"}}>Search</Button>
+
+            </form>
+            <label>{answer}</label>
+            </Container>
+         </div>         
+         <br/>
+         <br/>
+         <br/>
+         <br/>
       </div>
    );
 }
